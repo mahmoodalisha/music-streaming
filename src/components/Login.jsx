61d8e36e6e17cfbd88551c533b67c47534e23504
Alisha <142445013+mahmoodalisha@users.noreply.github.com>
2024-07-07 02:07:30 +0530

    Update Login.jsx
    updated the redirect uri after deployment on netlify

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 
-export default function Login () {
+export default function Login() {
   const handleClick = async () => {
-    const redirect_uri = 'http://localhost:3000/'
-    const api_uri = 'https://accounts.spotify.com/authorize'
+    const redirect_uri = process.env.NODE_ENV === 'production' ? 'https://clever-kashata-2f622b.netlify.app/' : 'http://localhost:3000/';
+    const api_uri = 'https://accounts.spotify.com/authorize';
     const scope = [
       'user-read-email',
       'user-read-private',
@@ -13,14 +13,15 @@ export default function Login () {
       'user-read-currently-playing',
       'user-read-recently-played',
       'user-top-read'
-    ]
-    const fetching_uri = `${api_uri}?client_id=${
-      process.env.REACT_APP_CLIENT_ID
-    }&redirect_uri=${redirect_uri}&scope=${scope.join(
-      ' '
-    )}&response_type=token&show_dialog=true`
-    window.location.href = fetching_uri
-  }
+    ];
+    const fetching_uri = `${api_uri}?client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=${redirect_uri}&scope=${scope.join(' ')}&response_type=token&show_dialog=true`;
+
+    // Log the fetching URI to the console for debugging
+    console.log(fetching_uri);
+
+    window.location.href = fetching_uri;
+  };
+
   return (
     <Container>
       <img
@@ -29,8 +30,9 @@ export default function Login () {
       />
       <button onClick={handleClick}>Connect Spotify</button>
     </Container>
-  )
+  );
 }
+
 const Container = styled.div`
   display: flex;
   flex-direction: column;
@@ -52,4 +54,5 @@ const Container = styled.div`
     font-size: 1.4rem;
     cursor: pointer;
   }
-`
+`;
+
